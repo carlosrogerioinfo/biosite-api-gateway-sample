054f041ea88e5166e5e6061a8b123ded1b2f46f4
Carlos Rogerio <carlosrogerio.info@gmail.com>
2022-07-09 13:49:46 -0300

    Senha

--- src/building blocks/Biosite.Core/Runtime.cs
+++ src/building blocks/Biosite.Core/Runtime.cs
@@ -2,7 +2,7 @@
 {
     public static class Runtime
     {
-        public static string ConnectionString = "Server=biosite.mssql.somee.com;Database=biosite;User ID=carlospereira_SQLLogin_1;Password=48kabd7zri;";
+        public static string ConnectionString = "Server=server-data-base;Database=database;User ID=user-database;Password=database-password";
         public static string JwtSecretKey = "T`XN;|OL'T_.-cr5151t3Vfztyi[sHle}6eCE]F>Ek]o/-5727-6450244d15-b787-c6bbbb-ZU2o!>+Y~=D;%Y;ox3/=(_M=x0S";
         public static string Audience = "https://homolog-apix.Biosite.com/";
         public static string Issuer = "https://homolog-apix.Biosite.com/";
